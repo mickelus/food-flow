04f6fd513811224bf8c3d7be50c53429025dd732
Mikael E. Wikner <wikner.mikael@gmail.com>
2014-12-06 19:02:48 +0100

    indentation, mobile css

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 
 document.querySelector('#progress').style.height="23px";
+
+window.addEventListener("load",function() {
+    // Set a timeout...
+    setTimeout(function(){
+        // Hide the address bar!
+        window.scrollTo(0, 1);
+    }, 0);
+});
