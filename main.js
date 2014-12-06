194f3f5b52409fef3d140314ed2ef8e1a915a74a
Mikael E. Wikner <wikner.mikael@gmail.com>
2014-12-06 21:19:58 +0100

    added add transition to box adding addition

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 scheduler.init(recipes.soup);
 
-window.addEventListener("load",function() {
-    // Set a timeout...
-    setTimeout(function(){
-        // Hide the address bar!
-        window.scrollTo(0, 1);
-    }, 0);
-});
-
 //Update progressBar
 scheduler.onChange(function() {
     var progress = scheduler.getProgress();
