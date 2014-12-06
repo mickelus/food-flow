4d47b9cfa2da826ee6dc13885a1d7a14eedec35e
Mikael E. Wikner <wikner.mikael@gmail.com>
2014-12-06 23:39:19 +0100

    added more screens

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ scheduler.onChange(function() {
     var progress = scheduler.getProgress();
     document.querySelector('#progress').style.height= (progress*99 + 1) + "%";    
 })
+
+
+$(".nextButton").click(function() {
+   console.log("hola chicos")
+    var current = $(".screen.current").addClass("past");
+    current.next().addClass("current");
+    current.removeClass("current");
+})
