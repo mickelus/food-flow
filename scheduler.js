b48ba89b455f5d37ecca10d6b584d2ecc17e9619
Erik Hedvall <erikwhedvall@gmail.com>
2014-12-06 17:36:41 +0100

    Add scheduler dummy interface

---
+++ scheduler.js
@@ -0,0 +1,17 @@
+var scheduler = {
+	completeStep: function(stepId) {
+
+	},
+	getCurrentStep: function() {
+		return null;
+	},
+	onChange: function(callback) {
+
+	},
+	getTimers: function() {
+		return [];
+	},
+	getProgress: function() {
+		return 0;
+	}
+};
